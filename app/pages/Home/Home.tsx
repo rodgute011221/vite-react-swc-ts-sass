836b56a1397f5ba49e7fb836d73819f8c9b964ce
Rod <rodgute011221@email.com>
2026-04-06 07:34:18 -0500

    init

---
+++ app/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+import styles from "./Home.module.scss";
+export default function Home() {
+  return (
+    <div className={styles.Home}>
+      <span className={styles.title}>Vite + React</span>
+    </div>
+  );
+}
